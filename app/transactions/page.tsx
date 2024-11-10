d01bfa503ed0c93b3c7f572cb76d12dfb31a5826
Davi <dp.junior1997@gmail.com>
2024-11-10 18:51:01 -0300

    feat: add transaction type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { ArrowDownUpIcon } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 
-export const TransactionsPage = () => {
-  return <h1>Hello world</h1>;
+export const TransactionsPage = async () => {
+  // acessar as transações do DB
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="space-y-6 p-6">
+      {/* Titulo e botao */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          <ArrowDownUpIcon />
+          Adicionar transação
+        </Button>
+      </div>
+      {/* Tabela de transações */}
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
